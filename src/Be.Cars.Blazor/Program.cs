b34db464feca9a92b109aac9411bd9da2713696a
bartengine27 <bart@engine27.be>
2024-02-15 21:26:46 +0100

    basic opentelemetry support
    Upgrade to .net 8

--- src/Be.Cars.Blazor/Program.cs
+++ src/Be.Cars.Blazor/Program.cs
@@ -1,10 +1,13 @@
-using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Metrics;
 using Serilog;
 using Serilog.Events;
+using System;
+using System.Threading.Tasks;
 
 namespace Be.Cars.Blazor;
 
@@ -29,11 +32,38 @@ public class Program
         {
             Log.Information("Starting web host.");
             var builder = WebApplication.CreateBuilder(args);
+            //builder.WebHost.UseKestrel();
             builder.Host.AddAppSettingsSecretsJson()
                 .UseAutofac()
                 .UseSerilog();
             await builder.AddApplicationAsync<CarsBlazorModule>();
-            var app = builder.Build();
+            //add telemetry
+            //https://community.abp.io/posts/asp.net-core-metrics-with-.net-8.0-1xnw1apc
+            //https://learn.microsoft.com/en-us/aspnet/core/log-mon/metrics/metrics?view=aspnetcore-8.0
+            //https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.Prometheus.AspNetCore/README.md
+            builder.Services.AddOpenTelemetry()
+                .WithMetrics(builder =>
+                {                    
+                    builder.AddAspNetCoreInstrumentation();
+                    //.net8 only https://github.com/open-telemetry/opentelemetry-dotnet/pull/4934
+                    builder.AddMeter("Microsoft.AspNetCore.Hosting");
+                    //.net8 only https://github.com/open-telemetry/opentelemetry-dotnet/pull/4934
+                    builder.AddMeter("Microsoft.AspNetCore.Server.Kestrel");
+                    builder.AddView("http.server.request.duration",
+                        new ExplicitBucketHistogramConfiguration
+                        {
+                            Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
+                    0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+                        });
+                    //builder.AddAspNetCoreInstrumentation();
+                    builder.AddPrometheusExporter();
+                    builder.AddConsoleExporter();
+                });
+            var app = builder.Build();            
+            //app.UseOpenTelemetryPrometheusScrapingEndpoint(
+            //    context => context.Request.Path == builder.Configuration["ApplicationInsights:Path"]
+            //    /*&& context.Connection.LocalPort == 5001*/);
+            app.MapPrometheusScrapingEndpoint();
             await app.InitializeApplicationAsync();
             await app.RunAsync();
             return 0;
